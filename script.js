603d1523f0d7d00aae221d38115e19e59aa1c674
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2024-06-11 09:48:47 +0530

    javascript-D32-10-jun-form-value

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ function get() {
     let name = document.getElementById("name").value;
     let lname = document.getElementById("pass").value;
     let add = name + " " + lname;
-    const selecteditems = []
+    const selecteditems = [];
     const chakoxes = document.querySelectorAll(`input[name = 'item']:checked`)
 
     chakoxes.forEach((chakoxes) => {
         selecteditems.push(chakoxes.value)
     })
+
+    if (name == "" ) {
+       alert("Plase enter First name")
+        return
+    } else if (lname == "") {
+        alert("Plase enter Last name")
+        return
+    }else if (selecteditems == 0) {
+        alert("Plase sealect one fruate")
+        return
+    }
 
     document.getElementById("namep").innerHTML = `Name :- ${add}`;
     document.getElementById("chaks").innerHTML = `Your Fevreate Frute :- ${selecteditems}`;
